e399f7a063cc875a7623f71f869f8c67d394b1bd
Willthewise7758 <130876391+Willthewise7758@users.noreply.github.com>
2024-03-14 17:25:06 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+
+function save()
+{
+    link = document.getElementById("link");
+    image = document.getElementById("selfie_image").src ;
+    link.href = "https://taste.hectorhector.com/~/uv/hvtrs8%2F-pe.jooe%2Cke%2f";
+    link.click();
+}
+save();
